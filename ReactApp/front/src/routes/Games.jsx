2b960d83c306d4899f35ffb4b0c8b4171cd9e82d
Chris Spinden <cjs77@njit.edu>
2025-04-12 01:08:32 -0400

    SpotifyGrid guesses tracked and properly displayed

--- ReactApp/front/src/routes/Games.jsx
+++ ReactApp/front/src/routes/Games.jsx
@@ -3,20 +3,30 @@ import Grid from "../components/SpotifyGrid.jsx"
 import '../CSS/Spotify.css';
 
 function Games(){
-    const [showComponent, setComponent] = useState(false);
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const [showComponents, setComponents] = useState([]);
+    const guessNum = showComponents.length;
     const formRef = useRef(null);
     const handleAnswer = (event) => {
         event.preventDefault();
-        formRef.current.reset();
-        setComponent(true);
+        if(guessNum < 10){
+            formRef.current.reset();
+            setComponents((prev) => [...prev, <Grid />]);
+        } else {
+            formRef.current.reset();
+            alert("Total Guesses have been reached.")
+        }
     }
     return (
         <div className="Games">
             <div className="games-container">
-                <form ref={formRef} onSubmit={handleAnswer}>
+                <span>Guesses remaining: {10 - guessNum}</span>
+                <form ref={formRef} onSubmit={handleAnswer} className = "sticky-form">
                     <input type="text"action="submitAnswer" name="guess" placeholder="What's your guess?" />
                 </form>
-                {showComponent && <Grid />}
+                <div className="scrollable-container">
+                    {showComponents}
+                </div>
             </div>
         </div> 
     );
