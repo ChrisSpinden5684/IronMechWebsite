175d3b15071834a5ddafd0be6dbe7a617d32fed6
Chris Spinden <cjs77@njit.edu>
2025-04-12 00:25:36 -0400

    SpotifyGrid will now appear after form submit

--- ReactApp/front/src/components/SpotifyGrid.jsx
+++ ReactApp/front/src/components/SpotifyGrid.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 import Box from "../components/Box.jsx"
 import '../CSS/Spotify.css';
 
+
 function SpotifyGrid(){
     return (
-        <div className="container">
-             <input type="text" name="guess" placeholder="What's your guess?" />
             <div className="grid-container">
                 <Box topic={"Debut: "} data={"1997"} />
                 <Box topic={"Members: "} data={"2"}/>
@@ -13,8 +12,7 @@ function SpotifyGrid(){
                 <Box topic={"Gender: "}data={"Male"} />
                 <Box topic={"Genre:  "} data={"Country"}/>
                 <Box topic={"Country: "} data={"USA"}/>
-            </div>
-        </div>    
+            </div>   
     );
 }
 export default SpotifyGrid;
--- ReactApp/front/src/routes/Games.jsx
+++ ReactApp/front/src/routes/Games.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState, useRef } from 'react';
 import Grid from "../components/SpotifyGrid.jsx"
 import '../CSS/Spotify.css';
 
 function Games(){
+    const [showComponent, setComponent] = useState(false);
+    const formRef = useRef(null);
+    const handleAnswer = (event) => {
+        event.preventDefault();
+        formRef.current.reset();
+        setComponent(true);
+    }
     return (
         <div className="Games">
-            <Grid />
+            <div className="games-container">
+                <form ref={formRef} onSubmit={handleAnswer}>
+                    <input type="text"action="submitAnswer" name="guess" placeholder="What's your guess?" />
+                </form>
+                {showComponent && <Grid />}
+            </div>
         </div> 
     );
 }
